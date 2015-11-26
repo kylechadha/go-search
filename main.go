7d56c619f100d296c2bbbb6378e62040b45914d5
Kyle Chadha <kyle.chadha@gmail.com>
2015-11-25 22:27:47 -0500

    Add writeFile method

--- main.go
+++ main.go
@@ -10,10 +10,22 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"text/tabwriter"
 	"time"
 
 	"github.com/jaytaylor/html2text"
 )
+
+// For everything
+// check variable names
+// check comments
+// check logs
+// check all errors are handled
+// add readme
+
+// write tests :)
+// test with -race (specifically using the same slice of maps)
+// Need to set up error handling story
 
 // Define the maximum number of concurrent requests to be executed.
 var maxReqs = 20
@@ -25,49 +37,55 @@ type result struct {
 	err   error
 }
 
+// use timehop log package?
+// then can add a verbose flag
 func main() {
-	// Record that start time of execution.
+
+	// Record the start time of execution.
 	start := time.Now()
 
-	// Define and parse flags for input and output files and the search term.
-	urlsFile := flag.String("input", "urls.txt", "location of urls.txt")
+	// Define flags for the input file and search term.
+	urlsFile := flag.String("input", "urls.txt", "location of file containing urls")
 	term := flag.String("search", "", "search term")
 	flag.Parse()
 
-	// Read the urls file and return a slice of urls.
-	urls := readUrls(*urlsFile)
+	// Read the input file and return a slice of urls.
+	urls, err := readFile(*urlsFile)
+	if err != nil {
+		// ** try a format here that you can add a message too
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	// Pass the search term as well as the list of urls to the 'search' method.
+	// Pass the search term and slice of urls to the search method.
 	// We remove the first item from the slice, as it represents the column name.
 	results := search(*term, urls[1:])
 
-	for _, result := range results {
-		if result.err != nil {
-			log.Printf("site:%s found:%t err:%s\n", result.site, result.found, result.err.Error())
-		} else {
-			log.Printf("site:%s found:%t err:%v\n", result.site, result.found, result.err)
-		}
+	// Pass the results to the writeFile method.
+	err = writeFile(results)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	// Log the total time taken by the search.
+	// Log the total execution time of the application.
 	log.Printf("Search took %s", time.Since(start))
 }
 
-func readUrls(file string) []string {
+// readFile takes the file path of a csv file containing URLs in the second column,
+// and returns a slice of URLs.
+func readFile(path string) ([]string, error) {
 
-	csvfile, err := os.Open(file)
+	f, err := os.Open(path)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
-	defer csvfile.Close()
+	defer f.Close()
 
-	r := csv.NewReader(csvfile)
-
+	r := csv.NewReader(f)
 	rawData, err := r.ReadAll()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
 
 	var urls []string
@@ -75,29 +93,65 @@ func readUrls(file string) []string {
 		urls = append(urls, row[1])
 	}
 
-	return urls
+	return urls, nil
 }
 
-// write tests :)
-// test with -race (specifically using the same slice of maps)
-// Need to set up error handling story
-func search(term string, urls []string) map[string]result {
+func writeFile(results []result) error {
 
+	f, err := os.Create("results.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := new(tabwriter.Writer)
+
+	// Format in tab-separated columns with a tab stop of 4 (the default of most text editors).
+	w.Init(f, 0, 4, 0, '\t', 0)
+
+	fileContents := "Site\tFound\tError\t\n"
+	for _, result := range results {
+		if result.err != nil {
+			log.Printf("site:%s found:%t err:%s\n", result.site, result.found, result.err.Error())
+			fileContents += fmt.Sprintf("%s\t%t\t%s\n", result.site, result.found, result.err.Error())
+		} else {
+			log.Printf("site:%s found:%t err:%v\n", result.site, result.found, result.err)
+			fileContents += fmt.Sprintf("%s\t%t\t%v\n", result.site, result.found, result.err)
+		}
+	}
+
+	n, err := fmt.Fprintf(w, fileContents)
+	if err != nil {
+		return err
+	}
+
+	w.Flush()
+
+	log.Printf("%d bytes written to results.txt", n)
+
+	return nil
+}
+
+func search(term string, urls []string) []result {
+
+	// If no search term was provided, exit.
 	if term == "" {
+		// fmt.Errorf here? when is that used?
 		fmt.Println("No search term was provided. Expected arguments: '-search=searchTerm'.")
 		os.Exit(1)
 	} else {
+		// Lowercase the search term.
 		term = strings.ToLower(term)
 	}
 
-	// If there are less than 20 urls, decrease maxReqs to the number of urls.
-	// This way we don't spin up unnecessary goroutines.
+	// If there are less than 20 urls, decrease maxReqs to the number of urls
+	// to avoid spinning up unnecessary goroutines.
 	if maxReqs > len(urls) {
-		log.Printf("changing maxReqs to: %d", len(urls))
+		log.Printf("Changing maxReqs to: %d", len(urls))
 		maxReqs = len(urls)
 	}
 
-	// Create one chan of strings, on which we will pass urls (as work).
+	// Create one chan of strings, on which we will send work to be processed (urls).
 	// Create one chan of type result, on which we will return results.
 	// Set up a WaitGroup so we can track when all goroutines have finished processing.
 	ch := make(chan string)
@@ -120,31 +174,17 @@ func search(term string, urls []string) map[string]result {
 					return
 				}
 
-				// u, err := url.Parse("http://bing.com/search?q=dotnet")
-				// if err != nil {
-				// 	log.Printf("%s", err)
-				// 	os.Exit(1)
-				// }
-
 				response, err := client.Get("http://" + site)
-				// response, err := http.Get("http://" + site)
 				if err != nil {
-					// perhaps you should check here that the err was specifically 'no such host'
-					// can also be Client.Timeout exceeded while awaiting headers or well, any timeout
+					// If there errors, we'll try again with the 'www' prefix.
 					log.Printf("%s\n", err)
 					log.Printf("Initial request failed for %s, attempting 'www' prefix.", site)
 
-					// timeout := time.Duration(30 * time.Second)
-					// client := http.Client{
-					// 	Timeout: timeout,
-					// }
 					response, err = client.Get("http://www." + site)
-					// response, err = http.Get("http://www." + site)
 				}
 
 				if err != nil {
 					log.Printf("Both requests failed for %s, returning an error.", site)
-					// can you pass nil for bool instead of false?
 					done <- result{site, false, err}
 					continue
 				}
@@ -163,11 +203,6 @@ func search(term string, urls []string) map[string]result {
 					// os.Exit(1)
 				}
 
-				// two things
-				// figure out whether you're reading all properly (find an article on understanding http client / transport all this jazz)
-				// and figure out whether you can use httpclient or roll your own to avoid the wrong types of timeouts
-
-				// since we're reusing clients, will want to make sure we readall... may need to implement previous way back
 				text, err := html2text.FromString(string(contents))
 				// text, err := html2text.FromReader(response.Body)
 				if err != nil {
@@ -176,8 +211,7 @@ func search(term string, urls []string) map[string]result {
 					continue
 				}
 
-				text = strings.ToLower(text)
-				found := strings.Contains(text, term)
+				found := strings.Contains(strings.ToLower(text), term)
 				done <- result{site, found, nil}
 			}
 		}()
@@ -194,25 +228,18 @@ func search(term string, urls []string) map[string]result {
 		}
 	}()
 
-	results := make(map[string]result)
-	// add a timeout here
+	results := []result{}
 	for i := 0; i < len(urls); i++ {
 		select {
 		case result := <-done:
 			log.Printf("receiving result: %s", result.site)
-			results[result.site] = result
+			results = append(results, result)
 		}
 	}
 
-	log.Println("closing channel")
+	// Close the channel as a signal to the goroutines that no additional work will be processed.
 	close(ch)
-	// close(done)
 	wg.Wait()
 
 	return results
 }
-
-// is this going to fetch AND search or just fetch? if AND search, then it needs the term .. if just search, than you have to return the respond.Body? dicey
-// func fetch(url string, c chan result) {
-
-// }
